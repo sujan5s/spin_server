1e50f3a433ae856e2facf0003a8bac5872e514e0
Sujan S <sujanshivananda5@gmail.com>
2026-02-27 00:19:58 +0530

    Add .gitignore and untrack build/env files

--- dist/routes/referral.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-// import { getReferralStats } from '../controllers/referral.controller';
-const router = (0, express_1.Router)();
-// router.get('/', getReferralStats);
-exports.default = router;
